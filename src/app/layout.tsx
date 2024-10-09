48f70f51f3edd7b7067eb9375d3403e0b7c1a8d2
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2024-10-09 19:55:08 +0500

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
