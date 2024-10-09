51cfe2a6ab7248a3e5e665f268a2a7a2a5440cda
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2024-10-09 19:55:41 +0500

    page.tsx

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "../Navbar/page";
 export default function Register() {
   return (
     <div>
-      <Navbar />
+      
       <div className="bg-green-500 my-5">
         <h1 className="text-center text-2xl my-5">Register Page Data</h1>
         <p className="text-center text-red-900">
